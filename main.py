c0dc8561ad7abd565f024c94f45cd057cb196959
Rio <111706275+YargleWithWings@users.noreply.github.com>
2024-03-25 13:16:36 -0400

    initial project creation

---
+++ main.py
@@ -0,0 +1,5 @@
+from solver import Solver
+
+if __name__ == '__main__':
+    solver = Solver()
+    solver.solve()
---
+++ readme.md
@@ -0,0 +1,2 @@
+This is my attempt at solving any given nyt strands puzzle
+Uses a list of all possible words
