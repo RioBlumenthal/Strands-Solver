1f7f7e945472686155b64a6f216cada00ed89190
Rio <111706275+YargleWithWings@users.noreply.github.com>
2024-03-25 17:21:53 -0400

    now successfully opens strands

--- solver.py
+++ solver.py
@@ -1,6 +1,20 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
+
+start_button_class = 'Feo8La_playButton'
+close_button_class = 'PwGt5a_closeX'
 
 driver = webdriver.Chrome()
-driver.get("http://google.com")
+driver.get("https://www.nytimes.com/games/strands")
+
+start_button = WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, start_button_class)))
+start_button.click()
+
+close_button = WebDriverWait(driver, 2).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, close_button_class)))
+close_button.click()
 
 print(driver.title)
+
+driver.quit()
