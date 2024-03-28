ec91982f2897569d3d880507234a543975365836
Rio <111706275+YargleWithWings@users.noreply.github.com>
2024-03-27 22:19:25 -0400

    slightly tweaked formatting

--- solver.py
+++ solver.py
@@ -3,9 +3,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 
+import nltk
+from nltk.corpus import words
+
 start_button_class = 'Feo8La_playButton'
 close_button_class = 'PwGt5a_closeX'
 
+# launch the browser and click through start screen to get to puzzle
 driver = webdriver.Chrome()
 driver.get("https://www.nytimes.com/games/strands")
 
@@ -15,6 +19,7 @@ start_button.click()
 close_button = WebDriverWait(driver, 2).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, close_button_class)))
 close_button.click()
 
-print(driver.title)
+# get the puzzle
+
 
 driver.quit()
