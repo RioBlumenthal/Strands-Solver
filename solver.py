334a6e5c6703c81a2623ba6c6f85d47ad311e871
Rio <111706275+YargleWithWings@users.noreply.github.com>
2024-03-29 13:45:58 -0400

    Successfully gets the board as a 2d array

--- solver.py
+++ solver.py
@@ -6,8 +6,17 @@ from selenium.webdriver.common.by import By
 import nltk
 from nltk.corpus import words
 
+def printBoard(board):
+   for row in board:
+      for letter in row:
+         print(letter, end=' ')
+      print()
+      
 start_button_class = 'Feo8La_playButton'
 close_button_class = 'PwGt5a_closeX'
+board_class = 'UOpmtW_board'
+ROWS = 8
+COLS = 6
 
 # launch the browser and click through start screen to get to puzzle
 driver = webdriver.Chrome()
@@ -20,6 +29,15 @@ close_button = WebDriverWait(driver, 2).until(expected_conditions.presence_of_el
 close_button.click()
 
 # get the puzzle
+board_from_web = WebDriverWait(driver, 2).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, board_class)))
+board = [[0 for x in range(COLS)] for y in range(ROWS)]
 
+buttonAt = 0
+for x in range(ROWS):
+   for y in range(COLS):
+      board[x][y] = board_from_web.find_element(By.ID, f'button-{buttonAt}').text[0]
+      buttonAt += 1
+
+printBoard(board)
 
 driver.quit()
