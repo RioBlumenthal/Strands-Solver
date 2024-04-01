1261de308a8a6a8a38d2641a5f89b613d54d5bcb
Rio <111706275+YargleWithWings@users.noreply.github.com>
2024-04-01 01:12:19 -0400

    fucked up the browser trying to make it headless

--- solver.py
+++ solver.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
 
 from nltk.corpus import words
 
@@ -24,12 +26,18 @@ board_class = 'UOpmtW_board'
 ROWS = 8
 COLS = 6
 
+# path to chromedriver
+CHROMEDRIVER_PATH = 'C:/Users/Rio/OneDrive/Documents/Python happies/chromedriver.exe'
+
 # min/max length of words
 MIN_WORD_LENGTH = 4
 MAX_WORD_LENGTH = 12
 
+
 # launch the browser and click through start screen to get to puzzle
-driver = webdriver.Chrome()
+options = Options()
+options.add_argument("--headless")
+driver = webdriver.Chrome(options=options, executable_path=CHROMEDRIVER_PATH)
 driver.get("https://www.nytimes.com/games/strands")
 
 start_button = WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, start_button_class)))
@@ -46,6 +54,9 @@ for x in range(ROWS):
    for y in range(COLS):
       board[x][y] = board_from_web.find_element(By.ID, f'button-{buttonAt}').text[0].lower()
       buttonAt += 1
+
+# close the web browser
+driver.quit()
 
 # get a list of all english words from nltk
 english_words = [word.lower() for word in words.words() if word.isalpha()]
@@ -77,10 +88,8 @@ def findWordsHelper(board, trie, x, y, visited, word, words):
          for j in range(-1, 2):
             if x + i >= 0 and x + i < ROWS and y + j >= 0 and y + j < COLS and not visited[x + i][y + j]:
                findWordsHelper(board, trie, x + i, y + j, visited, word + board[x + i][y + j], words)
-   visited[x][y] = False
+   visited[x][y] = False 
 
 # find all words in the board
 words = findWords(board, trie)
 print(words)
-
-driver.quit()
